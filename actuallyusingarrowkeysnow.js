2c4e42acac2a0e6e17f4443879a609b115ba64b8
aulin11 <47871940+aulin11@users.noreply.github.com>
2019-02-24 17:27:18 -0500

    Add files via upload

--- actuallyusingarrowkeysnow.js
+++ actuallyusingarrowkeysnow.js
@@ -5,8 +5,6 @@ let green1 = 000;
 let green2 = 000;
 let blue1 = 000;
 let blue2 = 000;
-let plinput1 = null;
-let plinput2 = null;
 let currentc1 = null;
 let currentc2 = null;
 let scream = 0;
@@ -21,63 +19,39 @@ let attacksright = [];
 let attacksleft = [];
 let hit1 = [];
 let hit2 = [];
-let newvar = null;
+const array1 = [document.getElementById("rleft"), document.getElementById("gleft"), document.getElementById("bleft")];
+const array2 = [document.getElementById("rright"), document.getElementById("gright"), document.getElementById("bright")];
+const pl1 = document.getElementById("player1");
+const pl2 = document.getElementById("player2");
 window.onload = function init(){
-    const pl1 = document.getElementById("player1");
-    const pl2 = document.getElementById("player2");
-    const array1 = [document.getElementById("rleft"), document.getElementById("gleft"), document.getElementById("bleft")];
-    const array2 = [document.getElementById("rright"), document.getElementById("gright"), document.getElementById("bright")];
     currentc1 = 1;
     currentc2 = 1;
     array1[currentc1].style.border = "thick solid #F1E538";
     array2[currentc2].style.border = "thick solid #F1E538";
-    const x = setInterval(changePlayers, 1000);
-    const y = setInterval(movePlayer1, 100);
-    const z = setInterval(movePlayer2, 100);
+    x = setInterval(changePlayers, 100);
+    y = setInterval(movePlayer1, 100);
+    z = setInterval(movePlayer2, 100);
 }
-window.onkeydown = function() {checkKeyboard()};
-function checkKeyboard(){
-    if(newvar == 38){
-        pl1input = 1;
-    }
-    else if(newvar == 40){
-        pl1input = 2;
-    } 
-    else if(newvar == 37){
-        pl1input = 3;
-    }
-    else if(newvar == 39){
-        pl1input = 4;
-    }
-    else{
-        pl1input = null;
-    }
-    if(newvar == 87){
-        pl2input = 1;
-    }
-    if(newvar == 68){
-        pl2input = 4;
-    }
-    if(newvar == 83){
-        pl2input = 2;
-    }
-    if(newvar == 65){
-        pl2input = 3;
-    }
-    else{
-        pl2input = null;
-    }
-    if(newvar == 13){
+
+let keypressed = {}
+window.onkeydown = function(event) {
+    keypressed[event.keyCode] = true;
+}
+
+window.onkeyup = function(event) {
+    keypressed[event.keyCode] = false;
+}
+
+function changePlayers(){
+    pl1.style.backgroundColor = "rgb(" + red1 + "," + green1 + "," + blue1 +")";
+    pl2.style.backgroundColor = 'rgb(' + red2 + ',' + green2 + ',' + blue2 +')';
+    if(keypressed[13] == true){
         endCreation();
     }
 }
-function changePlayers(){
-    player1.style.backgroundColor = 'rgb(' + red1 + ',' + green1 + '.' + blue1 +')';
-    player2.style.backgroundColor = 'rgb(' + red2 + ',' + green2 + '.' + blue2 +')';
-}
 function changeGreen(playerNum, right){
     if(right == true){
-        if(playerNum == player1){
+        if(playerNum == pl1){
             if(green1 + 10 < 255){
                 green1 = green1 + 10;
             }
@@ -89,7 +63,7 @@ function changeGreen(playerNum, right){
         }
     }
     else{
-        if(playerNum == player1){
+        if(playerNum == pl1){
             if(green1 - 10> 0){
                 green1 = green1 - 10;
             }
@@ -103,19 +77,19 @@ function changeGreen(playerNum, right){
 }
 function changeRed(playerNum, right){
     if(right == true){
-        if(playerNum == player1){
+        if(playerNum == pl1){
             if(red1 + 10 < 255){
                 red1 = red1 + 10;
             }
         }
-        else{
+        else if(playerNum == pl2){
             if(red2 + 10 < 255){
                 red2 = red2 + 10;
             }
         }
     }
     else{
-        if(playerNum == player1){
+        if(playerNum == pl1){
             if(red1 - 10 > 0){
                 red1 = red1 - 10;
             }
@@ -129,7 +103,7 @@ function changeRed(playerNum, right){
 }
 function changeBlue(playerNum, right){
     if(right == true){
-        if(playerNum == player1){
+        if(playerNum == pl1){
             if(blue1 + 10< 255){
                 blue1 = blue1 + 10;
             }
@@ -141,7 +115,7 @@ function changeBlue(playerNum, right){
         }
     }
     else{
-        if(playerNum == player1){
+        if(playerNum == pl1){
             if(blue2 - 10> 0){
                 blue2 = blue2 - 10;
             }
@@ -154,43 +128,43 @@ function changeBlue(playerNum, right){
     }
 }
 function movePlayer1(){
-    if (plinput1 == 1){
+    if (keypressed[38] == true){
         if(currentc1 != 0){
             array1[currentc1].style.border = "None";
             currentc1 = currentc1 - 1;
             array1[currentc1].style.border = "thick solid #F1E538";
         }
     }
-    else if(plinput1 == 2){
+    else if(keypressed[40] == true){
         if(currentc1 != 2){
             array1[currentc1].style.border = "None";
             currentc1 = currentc1 + 1;
             array1[currentc1].style.border = "thick solid #F1E538";
         }
     }
-    else if(plinput1 == 3){
+    else if(keypressed[37] == true){
         if(currentc1 == 0){
-            changeRed(player1, false);
+            changeRed(pl1, false);
         }
         else if(currentc1 == 1){
-            changeGreen(player1, false);
+            changeGreen(pl1, false);
         }
         else if(currentc1 == 2){
-            changeBlue(player1, false);
+            changeBlue(pl1, false);
         }
         else{
             currentc1 = 1;
         }
     }
-    else if(plinput1 == 4){
+    else if(keypressed[39] == true){
         if(currentc1 == 0){
-            changeRed(player1, true);
+            changeRed(pl1, true);
         }
         else if(currentc1 == 1){
-            changeGreen(player1, true);
+            changeGreen(pl1, true);
         }
         else if(currentc1 == 2){
-            changeBlue(player1, true);
+            changeBlue(pl1, true);
         }
         else{
             currentc1 = 1;
@@ -198,43 +172,43 @@ function movePlayer1(){
     }
 }
 function movePlayer2(){
-    if (plinput2 == 1){
+    if (keypressed[87] == true){
         if(currentc2 != 0){
-            array2[current2].style.border = "None";
+            array2[currentc2].style.border = "None";
             currentc2 = currentc2 - 1;
-            array2[current2].style.border = "thick solid #F1E538";
+            array2[currentc2].style.border = "thick solid #F1E538";
         }
     }
-    else if(plinput2 == 2){
+    else if(keypressed[83] == true){
         if(currentc2 != 2){
-            array2[current2].style.border = "None";
+            array2[currentc2].style.border = "None";
             currentc2 = currentc2 + 1;
-            array2[current2].style.border = "thick solid #F1E538";
+            array2[currentc2].style.border = "thick solid #F1E538";
         }
     }
-    else if(plinput2 == 3){
+    else if(keypressed[65] == true){
         if(currentc2 == 0){
-            changeRed(player2, false);
+            changeRed(pl2, false);
         }
           else if(currentc2 == 1){
-            changeGreen(player2, false);
+            changeGreen(pl2, false);
         }
         else if(currentc2 == 2){
-            changeBlue(player2, false);
+            changeBlue(pl2, false);
         }
         else{
             currentc2 = 1;
         }
     }
-    else if(plinput2 == 4){
+    else if(keypressed[68] == true){
         if(currentc2 == 0){
-            changeRed(player2, true);
+            changeRed(pl2, true);
         }
         else if(currentc2 == 1){
-            changeGreen(player2, true);
+            changeGreen(pl2, true);
         }
         else if(currentc2 == 2){
-            changeBlue(player1, true);
+            changeBlue(pl2, true);
         }
         else{
             currentc2 = 1;
@@ -245,19 +219,19 @@ function endCreation(){
     if(pl1.style.backgroundColor != pl2.style.backgroundColor){
         if(pl1.style.backgroundColor == "rgb(0, 0, 0)"){
             blue1 = blue1 + 10;
-            player1.style.backgroundColor = 'rgb(' + red1 + ',' + green1 + '.' + blue1 +')';
+            pl1.style.backgroundColor = 'rgb(' + red1 + ',' + green1 + ',' + blue1 +')';
         }
         if(pl2.style.backgroundColor == "rgb(0, 0, 0)"){
             blue2 = blue2 + 10;
-            player2.style.backgroundColor = 'rgb(' + red2 + ',' + green2 + '.' + blue2 +')';
+            pl2.style.backgroundColor = 'rgb(' + red2 + ',' + green2 + ',' + blue2 +')';
         }
         if(pl1.style.backgroundColor == "rgb(120, 120, 120)"){
             blue1 = blue1 + 10;
-            player1.style.backgroundColor = 'rgb(' + red1 + ',' + green1 + '.' + blue1 +')';
+            pl1.style.backgroundColor = 'rgb(' + red1 + ',' + green1 + ',' + blue1 +')';
         }
         if(pl2.style.backgroundColor == "rgb(120, 120, 120)"){
             blue2 = blue2 + 10;
-            player2.style.backgroundColor = 'rgb(' + red2 + ',' + green2 + '.' + blue2 +')';
+            pl2.style.backgroundColor = 'rgb(' + red2 + ',' + green2 + ',' + blue2 +')';
         }
         document.getElementsByClassName("chooseYourColor").display = "none";
         clearInterval(x);
@@ -274,7 +248,7 @@ function endCreation(){
         else{
             blue2 = blue2 - 10;
         }
-        player2.style.backgroundColor = 'rgb(' + red2 + ',' + green2 + '.' + blue2 +')';
+        pl2.style.backgroundColor = 'rgb(' + red2 + ',' + green2 + ',' + blue2 +')';
         clearInterval(x);
         clearInterval(y);
         clearInterval(z);
@@ -284,16 +258,20 @@ function endCreation(){
     }
 }
 function gameStart(){
-    pl1.style.top = 10;
-    pl1.style.left = 10;
-    pl2.style.bottom = 10;
-    pl2.style.right = 10;
+    pl1.style.top = "10px";
+    pl1.style.left = "10px";
+    pl2.style.bottom = "10px";
+    pl2.style.right = "10px";
+    document.getElementById("left").style.display = "none";
+    document.getElementById("right").style.display = "none";
     setStage();
 }
 function shoot(playerNum, direction){
-    if(playerNum == player1){
+    if(playerNum == pl1){
         let ack = document.getElementById("attackp1");
         clone = ack.cloneNode(true);
+        clone.style.display = 'block';
+        //clone.style.backgroundColor = 
         if(p1direction == 1){
             attacksup.push(clone);
             hit2.push(clone);
@@ -315,6 +293,7 @@ function shoot(playerNum, direction){
     else{
         let ack = document.getElementById("attackp2");
         clone = ack.cloneNode(true);
+        clone.style.display = 'block';
         if(p2direction == 1){
             attacksup.push(clone);
             hit1.push(clone);
@@ -334,56 +313,45 @@ function shoot(playerNum, direction){
         document.body.appendChild(clone);  
     }
 }
-function itsRaining(){
-    let pos = Math.floor(Math.random() * (100));
-    let aaaaa = document.getElementById("itsraining");
-    clone = aaaaa.cloneNode(true);
-    clone.style.left = pos + "%";
-    clone.style.top = "0px";
-    attacksdown.push(clone);
-    hit2.push(clone);
-    hit1.push(clone);
-    document.body.appendChild(clone);
-}
 function setStage(){
     p1shoot = setInterval(shoot(pl1, p1direction), 1000);
     p2shoot = setInterval(shoot(pl2, p2direction), 1000);
-    frames = setInterval(updateFrame(), 1000);
-    checkMissiles = setInterval(die(), 1000);
-    checkDamage = setInterval(oop(), 1000);
-    checkCollision = setInterval(oops(), 1000);
-    gdt = setInterval(helpme(),1000);
+    frames = setInterval(updateFrame, 1000);
+    checkMissiles = setInterval(die, 1000);
+    checkDamage = setInterval(oop, 1000);
+    checkCollision = setInterval(oops, 1000);
+    gdt = setInterval(helpme,1000);
 }
 function helpme(){
-    if(pl1input == 1){
+    if(keypressed[38] == true){
         p1direction = 1;
         pl1.style.top = pl1.style.top - 10;
     }
-    else if(pl1input == 2){
+    else if(keypressed[40] == true){
         p1direction = 2;
         pl1.style.top = pl1.style.top + 10;
     }
-    else if(pl1input == 3){
+    else if(keypressed[37] == true){
         p1direction = 3;
         pl1.style.left = pl1.style.left - 10;
     }
-    else if(pl1input == 4){
+    else if(keypressed[39] == true){
         p1direction = 4;
         pl1.style.left = pl1.style.left + 10;
     }
-    if(pl2input == 1){
+    if(keypressed[87] == true){
         p2direction = 1;
         pl2.style.top = pl2.style.top - 10;
     }
-    else if(pl2input == 2){
+    else if(keypressed[83] == true){
         p2direction = 2;
         pl2.style.top = pl2.style.top + 10;
     }
-    else if(pl2input == 3){
+    else if(keypressed[65] == true){
         p2direction = 3;
         pl2.style.left = pl2.style.left - 10;
     }
-    else if(pl2iput == 4){
+    else if(keypressed[68] == true){
         p2direction = 4;
         pl2.style.left = pl2.style.left + 10;
     }
@@ -414,62 +382,62 @@ function dotheyCollide(thing1, thing2) {
 function oop(){
     if(pl1.style.top == 0){
         p1lives = p1lives - 1;
-        pl1.style.top = pl1.style.top + 10;
+        pl1.style.top = pl1.style.top + "10px";
     }
     else if(pl1.style.left == 0){
         p1lives = p1lives - 1;
-        pl1.style.left = pl1.style.left + 10;
+        pl1.style.left = pl1.style.left + "10px";
     }
     else if(pl1.style.bottom == 0){
         p1lives = p1lives - 1;
-        pl1.style.bottom = pl1.style.bottom + 10;
+        pl1.style.bottom = pl1.style.bottom + "10px";
     }
     else if(pl1.style.right == 0){
         p1lives = p1lives -1;
-        pl1.style.right = pl1.style.right + 10;
+        pl1.style.right = pl1.style.right + "10px";
     }
 
     if(pl2.style.top == 0){
         p2lives = p2lives - 1;
-        pl2.style.top = pl2.style.top + 10;
+        pl2.style.top = pl2.style.top + "10px";
     }
     else if(pl2.style.left == 0){
         p2lives = p2lives - 1;
-        pl2.style.left = pl2.style.left + 10;
+        pl2.style.left = pl2.style.left + "10px";
     }
     else if(pl2.style.bottom == 0){
         p2lives = p2lives - 1;
-        pl2.style.bottom = pl2.style.bottom + 10;
+        pl2.style.bottom = pl2.style.bottom + "10px";
     }
     else if(pl2.style.right == 0){
         p2lives = p2lives -1;
-        pl2.style.right = pl2.style.right + 10;
+        pl2.style.right = pl2.style.right + "10px";
     }
 }
 function updateFrame(){
     for(i=0; i<attacksup.length; i++){
-        attacksup[i].style.top = attacksup[i].style.top - "10";
+        attacksup[i].style.top = attacksup[i].style.top - "10px";
     }
     for(i=0; i<attacksdown.length; i++){
-        attacksdown[i].style.down = attacksdown[i].style.top + "10";
+        attacksdown[i].style.down = attacksdown[i].style.top + "10px";
     }
     for(i=0; i<attacksleft.length; i++){
-        attacksleft[i].style.left = attacksleft[i].style.left - "10";
+        attacksleft[i].style.left = attacksleft[i].style.left - "10px";
     }
     for(i=0; i<attacksright.length; i++){
-        attacksright[i].style.left = attacksright[i].style.left + "10";
+        attacksright[i].style.left = attacksright[i].style.left + "10px";
     }
-    if(pl1input == 1){
-        pl1.style.top = pl1.style.top - "10"
+    if(keypressed[38] == true){
+        pl1.style.top = pl1.style.top - "10px"
     }
-    if(pl1input == 2){
-        pl1.style.top = pl1.style.top + "10"
+    if(keypressed[40] == true){
+        pl1.style.top = pl1.style.top + "10px"
     }
-    if(pl1input == 3){
-        pl1.style.top = pl1.style.top - "10"
+    if(keypressed[37] == true){
+        pl1.style.top = pl1.style.top - "10px"
     }
-    if(pl1input == 4){
-        pl1.style.top = pl1.style.top + "10"
+    if(keypressed[39] == true){
+        pl1.style.top = pl1.style.top + "10px"
     }
     if(p1lives == 0){
         GameOver();
@@ -481,22 +449,22 @@ function updateFrame(){
 function die(){
     for(i=0; i<attacksup.length; i++){
         if(attacksup[i].style.bottom == 0){
-            attacksup.style.display = "none";
+            attacksup[i].style.display = "none";
         }
     }
     for(i=0; i<attacksdown.length; i++){
         if(attacksdown[i].style.top == 0){
-            attacksdown.style.display = "none";
+            attacksdown[i].style.display = "none";
         }
     }
     for(i=0; i<attacksleft.length; i++){
         if(attacksleft[i].style.right == 0){
-            attacksleft.style.display = "none";
+            attacksleft[i].style.display = "none";
         }
     }
     for(i=0; i<attacksright.length; i++){
         if(attacksright[i].style.left == 0){
-            attacksright.style.display = "none";
+            attacksright[i].style.display = "none";
         }
     }
 }
