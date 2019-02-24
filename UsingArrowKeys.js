2f99ada38540e9a95803823fdda584d058d6aa34
aulin11 <47871940+aulin11@users.noreply.github.com>
2019-02-24 15:58:55 -0500

    Not using arrowkeys yet, this is the one we should use if we fix arduino

--- UsingArrowKeys.js
+++ UsingArrowKeys.js
@@ -36,7 +36,7 @@ window.onload = function init(){
     thingthatineedanamefor = setTimeout(endCreation(), 30000);
     const whynot = setInterval(checkKeyboard(), 100);
 }
-checkKeyboard(){
+function checkKeyboard(){
 
 }
 function changePlayers(){
